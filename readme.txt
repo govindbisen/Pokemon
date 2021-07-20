119192fb153e6e876350c8d5cd45b1e1055cecc8
govindbisen <31060771+govindbisen@users.noreply.github.com>
2021-07-20 19:40:57 +0530

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,4 @@
+ https://pokeapi.co/api/v2/{endpoint} 
+https://pokeapi.co/api/v2/pokemon/{endpoint} 
+
+‘https://pokeapi.co/api/v2/pokemon?limit=151',
---
+++ script.js
@@ -0,0 +1,33 @@
+const pokemon_container = document.getElementById("pokemon_container");
+const pokemon_number = 150;
+
+async function getData(){
+   try {
+    var data = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+    res = await data.json();
+    for(let i=0;i<pokemon_number;i++)
+    {   var div = document.createElement("div");
+        div.innerText=res.results[i].name;
+        div.style.fontSize = "40px";
+        div.style.color = "white";
+        div.style.textAlign = "center";
+
+        pokemon_container.appendChild(div);
+        
+        let pokemon_image = document.createElement('img');
+        pokemon_image.setAttribute("width",250)
+        pokemon_image.src="https://pokeres.bastionbot.org/images/pokemon/"+(i+1)+".png";
+        div.appendChild(pokemon_image);
+       
+    }
+    
+    
+   } catch (error) {
+       console.log(error);
+   }
+
+
+
+}
+
+getData();
